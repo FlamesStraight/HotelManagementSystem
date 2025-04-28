90f4428d6ef799a4f2a76aba2d24d64143ee2947
TristanL066 <135767095+TristanL066@users.noreply.github.com>
2025-04-28 21:34:15 +1200

    Update CustomerManager.java

--- src/CustomerManager.java
+++ src/CustomerManager.java
@@ -1,39 +1,12 @@
-import java.io.*;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class CustomerManager {
-    private Map<String, Customer> customers = new HashMap<>();
+    private Map<String, Customer> customers;
     private final Scanner scanner = new Scanner(System.in);
 
     public CustomerManager() {
-        loadCustomersData();
-    }
-
-    private void loadCustomersData() {
-        try(Scanner scanner = new Scanner(new File("customers.txt"))) {
-            while(scanner.hasNextLine()) {
-                String line = scanner.nextLine().trim();
-                if(!line.isEmpty()) {
-                    String[] customerDetails = line.split(",");
-
-                    if(customerDetails.length >= 3) {
-                        String fullName = customerDetails[0].trim();
-                        String email = customerDetails[1].trim();
-                        String phoneNumber = customerDetails[2].trim();
-
-                        Customer customer = new Customer(fullName, email, phoneNumber);
-                        customers.put(fullName.toLowerCase(), customer);
-                    }
-                    else{
-                        System.out.println("Saved customer details is invalid!");
-                    }
-                }
-            }
-        } catch(IOException e){
-            System.out.println("Error reading from customer file.");
-        }
+        customers = FileManager.loadCustomers();
     }
 
     public Customer checkCustomer(String fullName) {
@@ -46,14 +19,32 @@ public class CustomerManager {
             while(true){
                 String updateAnswer = checkInputValidity("\nWould you like to update it? (Y/N): ",false, false, true);
                 if(updateAnswer.equalsIgnoreCase("Y")) {
-                    String emailInput = checkInputValidity("\nUpdate your email: ", true, false, false);
-                    String phoneNumberInput = checkInputValidity("\nUpdate your phone number: ", false, true, false);
 
-                    customer.setEmail(emailInput);
-                    customer.setPhoneNumber(phoneNumberInput);
+                    while (true) {
+                        String emailInput = checkInputValidity("\n Update your email: ", true, false, false);
+                        if (emailInput.equalsIgnoreCase(customer.getEmail())) {
+                            System.out.println("\nError: New email must be different from your current email. Please try again...");
+                        }
+                        else {
+                            customer.setEmail(emailInput);
+                            break;
+                        }
+                    }
 
-                    overwriteCustomerData();
+                    while (true) {
+                        String phoneNumberInput = checkInputValidity("\n Update your phone number: ", false, true, false);
+                        if (phoneNumberInput.equalsIgnoreCase(customer.getPhoneNumber())) {
+                            System.out.println("\n Error: New phone number must be different from your current phone number. Please try again...");
+                        }
+                        else {
+                            customer.setPhoneNumber(phoneNumberInput);
+                            break;
+                        }
+                    }
+
+                    FileManager.updateCustomer(customers);
                     break;
+
                 }
                 else if(updateAnswer.equalsIgnoreCase("N")) {
                     break;
@@ -73,7 +64,7 @@ public class CustomerManager {
             customer = new Customer(fullName, emailInput, phoneNumberInput);
             customers.put(fullName.toLowerCase(), customer);
 
-            saveCustomersData(customer);
+            FileManager.addCustomer(customer);
         }
         return customer;
     }
@@ -84,7 +75,7 @@ public class CustomerManager {
             System.out.print(userInput);
             input = scanner.nextLine();
             if(input.isEmpty()){
-                System.out.println("No input provided, please try again...");
+                System.out.println("\nNo input provided, please try again...");
             }
             else{
                 return input;
@@ -106,7 +97,7 @@ public class CustomerManager {
                 }
             }
             if(aPhoneNumber){
-                if(input.matches("\\d+")){
+                if (input.matches("\\d{7,}")){
                     return input;
                 }
                 else{
@@ -120,29 +111,5 @@ public class CustomerManager {
                 System.out.println("\nInvalid input, please enter 'Y' or 'N'...");
             }
         }
-    }
-
-    private void saveCustomersData(Customer customer) {
-        try(FileWriter writer = new FileWriter("customers.txt", true)) {
-            writer.write(customer.toFileString() + "\n");
-            System.out.println("\nYour information has been saved.\n");
-        } catch (IOException e) {
-            System.out.println("Error occurred when saving customer data.");
-        }
-    }
-
-    private void overwriteCustomerData() {
-        String fileName = "customers.txt";
-        try (FileOutputStream fOut = new FileOutputStream(fileName);
-             PrintWriter pw = new PrintWriter(fOut)) {
-
-            for (Customer customer : customers.values()) {
-                pw.println(customer.toFileString());
-            }
-            System.out.println("\nYour information has been saved.\n");
-        } catch (IOException ex) {
-            System.out.println("Error occurred when saving customer data.");
-        }
-
     }
 }
