dc52145efbc798789df14cf420365f7b83038c82
TristanL066 <135767095+TristanL066@users.noreply.github.com>
2025-05-01 20:34:47 +1200

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -13,20 +13,20 @@ public class Customer {
         return name;
     }
 
-    public void setName(String name){
-        this.name = name;
-    }
-
     public String getEmail(){
         return email;
     }
 
-    public void setEmail(String email){
-        this.email = email;
-    }
-
     public String getPhoneNumber() {
         return phoneNumber;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
     }
 
     public void setPhoneNumber(String phoneNumber){
